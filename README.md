d17b2960c24492d7f283f70a1ba114f0e37f7fba
dinarayakupova <dinarayakupova@hotmail.com>
2024-05-17 21:45:08 -0400

    all project files uploaded

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # weather-dashboard
-Real-Time Weather Dashboard that provides users with current weather conditions for any city worldwide.
+Real-Time Weather Dashboard that provides users with current weather conditions for any city worldwide. Leveraging the OpenWeatherMap API, this application allows users to enter a city name and instantly receive detailed weather data, including temperature, feels-like temperature, weather conditions, and temperature variations throughout the day. The focus of this project was to deepen my understanding of API integration in JavaScript and enhance my skills in handling asynchronous data retrieval and updating the UI dynamically based on user input.
+
+### Before running the project, you need to get your own API key from OpenWeatherMap.
+
+## Features:
+* City-Based Weather Information
+* Real-Time Data Retrieval
+* Dynamic Date Handling
+* Responsive Design
+
+## Learning Outcomes:
+* API Integration: Learned how to make HTTP requests to an API to fetch data and handle responses asynchronously using JavaScript.
+* Dynamic UI Updates: Enhanced skills in manipulating the DOM to update the user interface dynamically based on the data received from the API.
+* Event Handling: Gained deeper insights into JavaScript event handling, particularly handling the 'Enter' key event for search functionality.
+* Date Manipulation: Demonstrated the ability to work with JavaScript’s Date object to display the current date, which updates dynamically each time the application is accessed.
+
+## Technologies Used:
+* HTML for structure
+* CSS for styling
+* JavaScript for logic, API calls, and DOM manipulations
+
+![alt text](image.png)
+
+## Link to the app:
+https://get-the-weather-report.glitch.me/
---
+++ script.js
@@ -0,0 +1,68 @@
+const api = {
+    //link to get the access
+    endpoint:"https://api.openweathermap.org/data/2.5/",
+     //API KEY
+    key: "c913d02ecc8d781f740a34ac2427f209" 
+    
+}
+
+const input = document.querySelector("#input");
+input.addEventListener("keydown", enter);
+
+function enter (e) {
+    if(e.keyCode === 13) {
+        getInfo (input.value);
+    }
+}
+
+async function getInfo (data) {
+   const res = await fetch(`${api.endpoint}weather?q=${data}&units=metric&appID=${api.key}`);
+   const result = await res.json();
+   displayResult(result);
+}
+
+function displayResult (result) {
+   let city = document.querySelector("#city");
+   city.textContent=`${result.name}, ${result.sys.country}`;
+
+   //DATE
+   getOurDate();
+
+   let temperature = document.querySelector("#temperature");
+   temperature.innerHTML=`${Math.round(result.main.temp)}<span>°</span>`;
+
+   let feelsLike = document.querySelector("#feelsLike");
+   feelsLike.innerHTML= "Feels like:" + `${Math.round(result.main.feels_like)} <span>°</span>`;
+
+   let condition = document.querySelector("#condition");
+   condition.textContent = `${result.weather[0].main}`;
+
+   let variation = document.querySelector("#variation"); 
+   variation.innerHTML="Min: "+`${Math.round(result.main.temp_min)}<span>°</span>` +  "  Max: "+`${Math.round(result.main.temp_max)}<span>°</span>`;
+
+}
+
+function getOurDate() {
+
+    //1 - Today's date
+    const myDate = new Date;
+    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+
+    //2 - Week day
+    let weekDay = document.querySelector("#date").innerHTML = days[myDate.getDay()];
+    
+    //3 - Date
+    let day=myDate.getDate();
+
+    //4 - Month
+    let month = months[myDate.getMonth()];
+   
+    //5 - Year
+    let year = myDate.getFullYear();
+
+    let todayDate = document.querySelector("#date");
+    todayDate.textContent = `${weekDay}` + " " + `${day}` + " " + `${month}` + " " + `${year}`;
+}
+   
